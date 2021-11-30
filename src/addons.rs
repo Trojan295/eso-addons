0b6d239907c6097d482d5055886f4199b2b8225e
Damian Czaja <trojan295@protonmail.com>
2021-11-30 16:51:14 +0100

    refactor, tests

--- src/addons.rs
+++ src/addons.rs
@@ -45,8 +45,6 @@ impl Manager {
             errors: vec![],
         };
 
-        let re = Regex::new(r"^.+(/.+){2}\.txt$").unwrap();
-
         for entry in WalkDir::new(&self.addon_dir) {
             let entry_dir = entry?;
             let file_path = entry_dir.path();
@@ -261,6 +259,30 @@ fn get_cdn_download_link(dom: &RcDom) -> Option<String> {
     }
 }
 
+pub fn get_download_url(addon_url: &str) -> Option<String> {
+    let fns = vec![
+        |url: &str| {
+            let re = Regex::new(r"^https://.*esoui\.com/downloads/info(\d+)-(.+)$").unwrap();
+            re.captures(url).map(|captures| {
+                format!(
+                    "https://www.esoui.com/downloads/download{}",
+                    captures[1].to_owned(),
+                )
+            })
+        },
+        |url: &str| Some("ok".to_owned()),
+    ];
+
+    for f in fns {
+        let url = f(addon_url);
+        if url.is_some() {
+            return url;
+        }
+    }
+
+    None
+}
+
 fn get_root_dir(path: &Path) -> PathBuf {
     match path.parent() {
         None => path.to_owned(),
@@ -268,5 +290,33 @@ fn get_root_dir(path: &Path) -> PathBuf {
             "" => path.to_owned(),
             &_ => get_root_dir(parent),
         },
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_download_link() {
+        let tests: Vec<(&str, Option<String>)> = vec![
+            (
+                "https://www.esoui.com/downloads/info1360-CombatMetrics",
+                Some("https://www.esoui.com/downloads/download1360".to_string()),
+            ),
+            (
+                "https://www.esoui.com/downloads/info1360-CombatMetrics.html",
+                Some("https://www.esoui.com/downloads/download1360".to_string()),
+            ),
+            //(
+            //    "https://www.esoui.com/downloads/fileinfo.php?id=2817",
+            //    Some("https://www.esoui.com/downloads/download2817".to_string()),
+            //),
+        ];
+
+        for test in tests {
+            let url = get_download_url(test.0);
+            assert!(url == test.1, "Got value: {:?}", url);
+        }
     }
 }
